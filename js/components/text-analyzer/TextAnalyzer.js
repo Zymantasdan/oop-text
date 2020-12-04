764b27964d92f070a4acf5791c065de6dd4130e1
Zymantasdan <z.dansevicius@gmail.com>
2020-12-04 13:25:07 +0200

    43min

--- js/components/text-analyzer/TextAnalyzer.js
+++ js/components/text-analyzer/TextAnalyzer.js
@@ -1,4 +1,14 @@
 class TextAnalizer {
+    constructor(text) {
+        this.text = text;
+    }
+
+    leterCounter() {
+        // console.log('skaiciuojame raides');
+        return this.text.length;
+
+    }
+
 
 }
 
--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@ import { TextAnalizer } from './components/text-analyzer/TextAnalyzer.js';
 // is kur paimsime duomenis TEXTAREA ir INPUT
 const Textarea = document.querySelector('textarea');
 const submit = document.querySelector('input');
-// isitraukia teksta
-let text = '';
+let text = ''; 
+
 
 // kai CLICK spausim koky funkcionaluma naudosim, apsirasysim funkcija {....}
-// EVENT - paspaudimo ivykis, gaunam informacija apie ivyki deka addEventListener, kadangi vienintelis ivykis skliausteliu nereikia ('click', (event) => {});
-submit.addEventListener('click', event => {
-    // jeigu paspaustas mygtukas issiuncia info 
-    event.preventDefault(); 
-    text = Textarea.value;
-    console.log(text);
+// EVENT - paspaudimo ivykis,(event gali buti bent kas) gaunam informacija apie ivyki deka addEventListener, kadangi vienintelis ivykis skliausteliu nereikia ('click', (event) => {});
 
+submit.addEventListener('click', event => {
+    event.preventDefault(); /* jeigu paspaustas  CLICK mygtukas issiuncia info, nustoja relaudinti kai paspaudi*/
+    text = Textarea.value; /* TEXT reiksme isitraukiam, (kai irasysim)*/
+    
+    const analizatorius = new TextAnalizer(text);
+    const leterCount = analizatorius.leterCounter(); /* isvestis is main 'skaiciuojame raides' */
+    console.log(analizatorius);  
+    console.log('raidziu kiekis :', leterCount);
+    
 });
